6e9f39393a116b6a1f8283b82942e8e5de3f2367
maryamzafar1 <30714111+maryamzafar1@users.noreply.github.com>
2017-08-06 17:43:00 +0500

    Update app.py

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ def processProjectName(req):
     return project_name 
 
 def storeSessIDandURL(sessID, url):
-    require(RCurl)
+    library(RCurl)
     nuts <-getURL("https://raw.githubusercontent.com/maryamzafar1/aarz1/master/sess_url.txt")
     nuts.write("{"+sessID+","+url+"}\n") 
     #with open("https://raw.githubusercontent.com/maryamzafar1/aarz1/master/sess_url.txt", "a") as fh: 
